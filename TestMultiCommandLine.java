80a1abcbd5db6599f0a8cb297f6b5cbe9f1c1f6f
HappyPatel18 <101093896+HappyPatel18@users.noreply.github.com>
2022-03-07 16:00:56 +0530

    Create TestMultiCommandLine.java

---
+++ TestMultiCommandLine.java
@@ -0,0 +1,11 @@
+package com.Myproject;
+import com.Myproject.MultiCommandLine;
+
+public class TestMultiCommandLine {
+
+    public static void main(String[] args) {
+        MultiCommandLine obj=new MultiCommandLine();
+        obj.doOpertaions(args);
+
+    }
+}
