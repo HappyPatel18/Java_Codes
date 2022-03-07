f5f96872d84c69287d2a40085ffa5de75540dc5b
HappyPatel18 <101093896+HappyPatel18@users.noreply.github.com>
2022-03-07 14:04:32 +0530

    Create First.java

---
+++ First.java
@@ -0,0 +1,12 @@
+package com.Myproject;
+
+public class First {
+    public void greet(){
+        System.out.println("hello Welcome to my code");
+    }
+
+    public static void main(String[] args) {
+        First obj=new First();
+        obj.greet();
+    }
+}
